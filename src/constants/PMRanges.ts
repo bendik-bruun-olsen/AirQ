ab366748d0ca272b8258ed7d5870e545402be536
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-16 20:33:03 +0100

    updated colors in PMRanges with root var colors

--- src/constants/PMRanges.ts
+++ src/constants/PMRanges.ts
@@ -6,79 +6,91 @@ export interface PMRangeInterface {
 }
 
 const PM25Ranges = [
-	{ name: "Good", minValue: 0, maxValue: 15.4, color: "#00e400", value: 1 },
+	{
+		name: "Good",
+		minValue: 0,
+		maxValue: 15.4,
+		color: "var(--good-green",
+		value: 1,
+	},
 	{
 		name: "Moderate",
 		minValue: 15.5,
 		maxValue: 40.4,
-		color: "#ffff00",
+		color: "var(--moderate-yellow)",
 		value: 1,
 	},
 	{
 		name: "Unhealthy for Sensitive Groups",
 		minValue: 40.5,
 		maxValue: 65.4,
-		color: "#ff7e00",
+		color: "var(--sensitive-orange)",
 		value: 1,
 	},
 	{
 		name: "Unhealthy",
 		minValue: 65.5,
 		maxValue: 150.4,
-		color: "#ff0000",
+		color: "var(--unhealthy-red)",
 		value: 1,
 	},
 	{
 		name: "Very Unhealthy",
 		minValue: 150.5,
 		maxValue: 250.4,
-		color: "#8f3f97",
+		color: "var(--very-unhealthy-purple)",
 		value: 1,
 	},
 	{
 		name: "Hazardous",
 		minValue: 250.5,
 		maxValue: 500.4,
-		color: "#7e0023",
+		color: "var(--hazardous-maroon)",
 		value: 1,
 	},
 ];
 
 const PM10Ranges = [
-	{ name: "Good", minValue: 0, maxValue: 54, color: "#00e400", value: 1 },
+	{
+		name: "Good",
+		minValue: 0,
+		maxValue: 54,
+		color: "var(--good-green)",
+		value: 1,
+	},
 	{
 		name: "Moderate",
 		minValue: 55,
 		maxValue: 154,
-		color: "#ffff00",
+		color: "var(--moderate-yellow)",
 		value: 1,
 	},
 	{
 		name: "Unhealthy for Sensitive Groups",
 		minValue: 155,
 		maxValue: 254,
-		color: "#ff7e00",
+		color: "var(--sensitive-orange)",
 		value: 1,
 	},
 	{
 		name: "Unhealthy",
 		minValue: 255,
 		maxValue: 354,
-		color: "#ff0000",
+		color: "var(--unhealthy-red)",
 		value: 1,
 	},
 	{
 		name: "Very Unhealthy",
 		minValue: 355,
 		maxValue: 424,
-		color: "#8f3f97",
+		color: "var(--very-unhealthy-purple)",
 		value: 1,
 	},
 	{
 		name: "Hazardous",
 		minValue: 425,
 		maxValue: 604,
-		color: "#7e0023",
+		color: "var(--hazardous-maroon)",
 		value: 1,
 	},
 ];
