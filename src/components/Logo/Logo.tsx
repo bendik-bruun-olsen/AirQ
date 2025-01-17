700984ddcc7b8c2b2e45a371adf23b53dcb59c8d
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-17 16:30:19 +0100

    Moved Logo comp into own folder, added css module

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import styles from "./Logo.module.css";
+
+export default function Logo() {
+	return (
+		<div className={styles.container}>
+			<span className={styles.title}>AirQ</span>
+			<span className={styles.slogan}>Your Air, Your Awareness</span>
+		</div>
+	);
+}
