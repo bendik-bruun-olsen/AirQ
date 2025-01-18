28fde15614945858a849b40e73683bbb68d68af6
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-18 13:30:00 +0100

    replaced status colors with more muted versions

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -8,8 +8,9 @@ interface Props {
 }
 
 export default function Summary({ data }: Props) {
-	const aqiStatus = getAqiStatus(data.aqi ?? -1);
-	const { status, color, icon } = aqiStatus;
+	const { status, color, icon } = getAqiStatus(data.aqi ?? -1);
+
+	console.log("data: ", data);
 
 	const findTimeSinceUpdate = (date: string) => {
 		const diff = new Date().getTime() - new Date(date).getTime();
@@ -29,8 +30,6 @@ export default function Summary({ data }: Props) {
 		return "just now";
 	};
 
-	console.log("DateSent: ", findTimeSinceUpdate(data.time.iso));
-
 	return (
 		<div className={styles.wrapper}>
 			<h2>{data.city.name}</h2>
@@ -40,6 +39,14 @@ export default function Summary({ data }: Props) {
 			<div className={styles.textContainer}>
 				<h3>Overall Air Quality:</h3>
 				<span className={styles.aqiValue}>{status}</span>
+			</div>
+			<div className={styles.textContainer}>
+				<h3>AQI Value:</h3>
+				<span className={styles.aqiValue}>{data.aqi}</span>
+			</div>
+			<div className={styles.textContainer}>
+				<h3>Temperature:</h3>
+				<span className={styles.aqiValue}>{data?.iaqi?.t?.v}℃</span>
 			</div>
 			<FontAwesomeIcon
 				icon={icon}
