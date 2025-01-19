63e94573303827b160d14e3f8438a4737534e74c
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-19 21:30:10 +0100

    Added selected location to localStorage

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -4,15 +4,16 @@ import {
 	useState,
 	Dispatch,
 	SetStateAction,
+	useEffect,
 } from "react";
 
 interface LocationContextType {
-	selectedLocation: { name: string; uid: string };
-	setSelectedLocation: Dispatch<SetStateAction<{ name: string; uid: string }>>;
+	selectedLocation: { uid: string };
+	setSelectedLocation: Dispatch<SetStateAction<{ uid: string }>>;
 }
 
 const LocationContext = createContext<LocationContextType>({
-	selectedLocation: { name: "", uid: "" },
+	selectedLocation: { uid: "" },
 	setSelectedLocation: () => {},
 });
 
@@ -21,10 +22,14 @@ interface Props {
 }
 
 function ContextProvider({ children }: Props) {
-	const [selectedLocation, setSelectedLocation] = useState({
-		name: "Kannik, Stavanger, Norway",
-		uid: "2657",
+	const [selectedLocation, setSelectedLocation] = useState(() => {
+		const storedLocation = localStorage.getItem("selectedLocation");
+		return storedLocation ? JSON.parse(storedLocation) : { uid: "2657" };
 	});
+
+	useEffect(() => {
+		localStorage.setItem("selectedLocation", JSON.stringify(selectedLocation));
+	}, [selectedLocation]);
 
 	return (
 		<LocationContext.Provider
