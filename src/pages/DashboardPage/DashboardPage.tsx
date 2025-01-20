e830a9db30e9d837251bbeb42c262c7f4499c363
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-20 12:42:45 +0100

    added button to change location/navigate to map page

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -8,15 +8,31 @@ import ForeCastGraph from "../../components/ForecastGraph/ForecastGraph";
 import HumidityChart from "../../components/HumidityChart/PieChart";
 import LocationSummary from "../../components/LocationSummary/LocationSummary";
 import AirQualitySummary from "../../components/AirQualitySummary/AirQualitySummary";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function DashboardPage() {
 	const { selectedLocation } = useContext(LocationContext);
 	const { data, isLoading } = useFetchData(selectedLocation.uid);
+	const navigate = useNavigate();
+
+	const handleChange = () => {
+		navigate("/map");
+	};
 
 	if (isLoading || data === null) return <LoadingPage />;
 	return (
 		<>
-			<h1>Dashboard</h1>
+			<div className={styles.headerContainer}>
+				<h1>Dashboard</h1>
+				<Button
+					variant="text"
+					onClick={handleChange}
+					sx={{ color: "var(--sky-blue)" }}
+				>
+					Change location
+				</Button>
+			</div>
 			<div className="divider" />
 			<div className={styles.wrapper}>
 				<div className={styles.itemContainer}>
